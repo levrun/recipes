7d27136af931413bc4e0275816423046e6630c93
Alexei Eraskin <levrun1@gmail.com>
2016-12-27 00:43:57 +0100

    fix toggle button problem

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,3],{415:function(n,o){},961:function(n,o,c){n.exports=c(415)}},[961]);
+webpackJsonp([2,3],{416:function(n,o){},964:function(n,o,c){n.exports=c(416)}},[964]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
